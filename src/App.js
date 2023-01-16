52d35dfa547209a3338a659900915b4253556199
Muhammad Danish Azeem <Muhammaddanish1458@gmail.com>
2023-01-16 21:55:59 +0500

    upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { MovieCard } from "./components/MovieCard";
+import "./App.css";
+import logo from "./assets/logo.png";
+import searchI from "./assets/search.svg";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const API_URL = "http://www.omdbapi.com/?apikey=d8cb3a1d";
+
+  const searchMovie = async (title) => {
+    let response = await fetch(`${API_URL}&s=${title}`);
+    let data = await response.json();
+    setMovies(data.Search);
+  };
+
+  const searchStyle = {
+    width: "40%",
+    height: "50px",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* Logo */}
+      <nav>
+        <div className="d-flex align-items-start justify-content-center sticky-top">
+          <img src={logo} alt="" width="369" height="250" />
+        </div>
+      </nav>
+      {/* End Logo */}
+      {/* Search Box */}
+      <div className="container d-flex align-items-center justify-content-center">
+        <div className="input-group m-3" style={searchStyle}>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search for Movies"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            aria-label="Search"
+            aria-describedby="button-addon2"
+          />
+          <button
+            className="btn btn-outline-warning"
+            type="button"
+            onClick={() => searchMovie(search)}
+          >
+            <img src={searchI} alt="" />
+          </button>
+        </div>
+      </div>
+      <br />
+      {/* End Search Box */}
+      {/* Movies Cards */}
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h3 className="text-center">Movies searched for: {search}</h3>
+          </div>
+        </div>
+      </div>
+      <br />
+      <div className="container">
+        <div className="row">
+          {movies === undefined ? (
+            <h3 className="text-center">No movies found</h3>
+          ) : (
+            movies.map((movie) => {
+              return (
+                <div className="col-3">
+                  <MovieCard movie={movie} />
+                </div>
+              );
+            })
+          )}
+        </div>
+      </div>
+
+      {/* End Movies Cards */}
+    </>
   );
 }
 
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const MovieCard = ({movie}) => {
+  const moviecardstyle={
+    height: "500px"
+  };
+  return (
+    <div className="container" style={moviecardstyle}>
+      <div className="card">
+        <img src={movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/400"} className="card-img-top" alt="" width="33%" height="250px" />
+        <div className="card-body">
+          <h6 className='card-subtitle mb-2 text-muted'>{movie.Type}</h6>
+          <h5 className="card-title">{movie.Title}</h5>
+          <p className="card-text">Released: {movie.Year}</p>
+          <a href={"https://www.imdb.com/title/"+movie.imdbID} target="_blank" rel='noreferrer' className="btn btn-warning">Go to IMDB</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
